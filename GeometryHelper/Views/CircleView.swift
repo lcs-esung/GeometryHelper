07dcb6d6a7159da214854e71be3935a79958e9f8
Edna Sung <esung@lcs.on.ca>
2024-01-15 11:36:38 -0500

    Now using a computed property from the Circle structure to show the diameter of the circle on screen.

--- GeometryHelper/Views/CircleView.swift
+++ GeometryHelper/Views/CircleView.swift
@@ -21,6 +21,9 @@ struct CircleView: View {
                 .font(.largeTitle)
             
             Slider(value: $currentCircle.radius, in: 0...100)
+            
+            Text("\(currentCircle.diameter)")
+                .font(.largeTitle)
         }
         .padding()
     }
